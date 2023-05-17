af7c1e34f47e1f0d5f057b4b1ff1453716b53fab
ritikchhipa01 <ritikchhipa01@gmail.com>
2023-05-17 22:56:38 +0530

    Uploaded file link -> email using Nodemailer

--- config/clodinary.js
+++ config/clodinary.js
@@ -1,20 +1,15 @@
 const cloudinary = require("cloudinary").v2;
-
 require("dotenv").config();
 
-exports.connectCloudinary = () =>{
-    try{
-        cloudinary.config({
-            // cloud_name: process.env.CLOUD_NAME,
-            // api_key: process.env.API_KEY,
-            // api_secret: process.env.CLOUD_NAME,
-            cloud_name: "ddifix5wn",
-            api_key: "664297994544247",
-            api_secret: "kMUVjAWALQxge06Up4L6eKm7uW4",
-        })
-
-    }
-    catch(error){
-        console.log(err);
-    }
-}
+exports.connectCloudinary = () => {
+  try {
+   
+    cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- model/File.js
+++ model/File.js
@@ -1,21 +1,47 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
+const fileSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+   },
+   imageUrl: {
+      type: String,
+   },
+   tags: {
+      type: String,
+   },
+   email: {
+      type: String,
+   },
+});
 
-const fileSchema =new mongoose.Schema({
-     name:{
-        type:String,
-        required: true,
-     },
-     imageUrl:{
-        type:String,
-     },
-     tags:{
-        type:String,
-     },
-     email:{
-        type:String,
-     }
-})
+//Post Middleware
+fileSchema.post("save", async function (DOC) {
+   try {
+      //transporter
+      let transport = nodemailer.createTransport({
+         host: process.env.MAIL_HOST,
+         auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+         },
+      });
 
-const file = mongoose.model("File",fileSchema);
+      //send Mail
+      const info = await transport.sendMail({
+         from: "Creative Pvt. Ltd.",
+         to: DOC.email,
+         subject: `Congratulation ${DOC.name}, Now you are a Creativians-_-`,
+         html: `<h2>HELLO ${DOC.name}</h2> <p>Good Evening</p> view Here: <a href="${DOC.imageUrl}">FILE LINK</a>`,
+      });
+   }
+   catch (error) {
+      console.log(error);
+   }
+});
+
+const file = mongoose.model("File", fileSchema);
 module.exports = file;
